63464e43d23a6729ab4cacbb05703aec5f1e41e9
Segus3088 <segus3088@gmail.com>
2016-06-20 17:30:59 -0500

    mostrando la salida

--- practica2.py
+++ practica2.py
@@ -241,6 +241,12 @@ def keyPressed(key,x,y):
 	#mover Eye y ++
 	if(key[0]=="k"):
 		varEyey -= 1
+	#mover Eye z ++
+	if(key[0]=="i"):
+		varEyez += 1
+	#mover Eye z --
+	if(key[0]=="o"):
+		varEyez -= 1
 	#Imprimir la config
 	print "upx: %i upy:%i upz:%i eyex:%i eyey:%i eyez%i centx:%i centy:%i centz:%i" % 
 	(varUPx, varUPy, varUPz, varEyex, varEyey, varEyez, varCentx, varCenty, varCentz)
